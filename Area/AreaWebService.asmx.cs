bf8ee1cffa1efae8c615ecf8b3ad1538ad16a079
rodo585 <61889939+rodo585@users.noreply.github.com>
2022-09-28 19:53:44 -0500

    Add files via upload

---
+++ Area/AreaWebService.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Area
+{
+    /// <summary>
+    /// Descripción breve de AreaWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AreaWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public decimal cuadrado(decimal n1)
+        {
+            SacarArea s = new SacarArea();
+            s.numero1 = n1;
+            return s.cuadrado(n1);
+        }
+        [WebMethod]
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            SacarArea s = new SacarArea();
+            s.numero1 = n1;
+            s.numero2 = n2;
+            return s.triangulo(n1,n2);
+        }
+        [WebMethod]
+        public double circulo(double n3)
+        {
+            SacarArea s = new SacarArea();
+            s.numero3 = n3;
+            return s.circulo(n3);
+        }
+    }
+}
---
+++ Area/SacarArea.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Area
+{
+    public class SacarArea
+    {
+        public decimal numero1 { get; set; }
+        public decimal numero2 { get; set; }
+        public double numero3 { get; set; }
+        public decimal cuadrado(decimal n1)
+        {
+            var resultado = n1 * n1;
+            return resultado;
+        }
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            var resultado = (n1 * n2)/2;
+            return resultado;
+        }
+        public double circulo(double n1)
+        {
+            const double pi = 3.1416;
+            double resultado = pi * (n1 * n1);
+            return resultado; 
+        }
+    }
+}
